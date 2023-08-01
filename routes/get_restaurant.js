dc9c48bc9e602f379a67c810e12c98473c69a44c
berke-cura <berkecura@protonmail.com>
2023-08-01 22:32:14 +0300

    subquery added

--- routes/get_restaurant.js
+++ routes/get_restaurant.js
@@ -19,34 +19,33 @@ router.post(
     const { latitude, longitude } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res
-        .status(400)
-        .json({
-          error: errors.errors.map((error) => error.msg),
-          message: "Example request",
-          body: { latitude: 41.08354, longitude: 29.04907 },
-        });
+      return res.status(400).json({
+        error: errors.errors.map((error) => error.msg),
+        message: "Example request",
+        body: { latitude: 41.08354, longitude: 29.04907 },
+      });
     }
 
     try {
       const result = await pool.query(
         `
-    SELECT id, name, latitude, longitude,
-    (
-      6371 * 
-      acos(
-        cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) +
-        sin(radians($1)) * sin(radians(latitude))
-      ) * 1000
-    ) AS distance
-    FROM restaurant
-    WHERE (
-      6371 * 
-      acos(
-        cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) +
-        sin(radians($1)) * sin(radians(latitude))
-      ) 
-    ) <= 10
+    SELECT id, name, latitude, longitude, distance
+    FROM (
+      SELECT
+        id,
+        name,
+        latitude,
+        longitude,
+        (
+          6371 * 
+          acos(
+            cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) +
+            sin(radians($1)) * sin(radians(latitude))
+          ) * 1000
+        ) AS distance
+      FROM restaurant
+    ) AS distance_calculation
+    WHERE distance <= 10000
     ORDER BY distance
     LIMIT 5;
   `,
@@ -56,7 +55,9 @@ router.post(
       const closestRestaurants = result.rows;
 
       if (closestRestaurants.length === 0) {
-        return res.status(204).json({ message: "There are no restaurants close to you." });
+        return res
+          .status(204)
+          .json({ message: "There are no restaurants close to you." });
       }
 
       closestRestaurants.forEach((restaurant) => {
